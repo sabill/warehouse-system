6e02ec19fd880fb14256f45828362fa639b73f83
@Sabill <sabila.hadinn245@gmail.com>
2019-11-11 15:04:00 +0700

    add

---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Sistem Informasi Warehouse</h1>
+    <form action="", method="get">
+        <img src="" alt="">
+        Username: <input type="text", name = username><br>
+        Password: <input type="text", name = password><br>
+        <input type="submit", value="Submit">
+    </form>
+</body>
+</html>
