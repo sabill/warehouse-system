ba07b7a4a95e9c949d88d4c22158ee5da07b81ed
sabill <44560502+sabill@users.noreply.github.com>
2019-12-05 23:57:15 +0700

    update about.php

---
+++ application/views/about.php
@@ -0,0 +1,79 @@
+<style>
+    .social-link {
+  width: 30px;
+  height: 30px;
+  border: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666;
+  border-radius: 50%;
+  transition: all 0.3s;
+  font-size: 0.9rem;
+}
+
+.social-link:hover,
+.social-link:focus {
+  background: #ddd;
+  text-decoration: none;
+  color: #555;
+}
+</style>
+
+<div class="bg-light py-10">
+  <div class="container py-5">
+    <div class="row mb-4">
+      <div class="col-lg-12">
+        <h2 class="display-4 font-weight-light">Tentang Kami</h2>
+        <p class="font-italic text-muted">Project ini adalah tugas pembuatan Sistem Informasi Warehouse Online berbasis web yang dikembangkan pada mata kuliah Proyek Multidisiplin. Pada Proyek ini kami juga bekerjasama dengan Prodi Logistik.</p>
+      </div>
+    </div>
+
+    <div class="row text-center">
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Birgitta Sekar Winda</h5>
+          <span class="small text-uppercase text-muted">105216009</span>
+          <br>
+          <span class="small text-uppercase text-muted">Project Manager</span>
+        </div>
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="<?php echo base_url('assets/images/sabila.png')?>" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Sabila Hadinnisa</h5>
+          <span class="small text-uppercase text-muted">105216002</span>
+          <br>
+          <span class="small text-uppercase text-muted">Front-End Developer</span>
+        </div>
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834130/avatar-3_hzlize.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Farhan Akbar</h5>
+          <span class="small text-uppercase text-muted">105216036</span>
+          <br>
+          <span class="small text-uppercase text-muted">Back-End Developer</span>
+        </div>
+      </div>
+      <!-- End-->
+
+      <!-- Team item-->
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-1_s02nlg.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+          <h5 class="mb-0">Akbar Nurcahyo</h5>
+          <span class="small text-uppercase text-muted">105216050</span>
+          <br>
+          <span class="small text-uppercase text-muted">Back-End Developer</span>
+        </div>
+      </div>
+      <!-- End-->
+
+    </div>
+  </div>
+</div>
