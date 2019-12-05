9661c527315b578fcf7c2793e92805367590cec4
sabill <44560502+sabill@users.noreply.github.com>
2019-12-06 00:06:32 +0700

    Update Dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,5 +1,5 @@
 <div class="col-md-10">
-    <div class="container-fluid">
+    <div class="container col-sm-12">
         <ol class="breadcrumb">
             <li class="breadcrumb-item">
                 <a href="#">Dashboard</a>
@@ -12,23 +12,23 @@
 				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 					<div class="panel panel-teal panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-shopping-cart color-blue"></em>
-							<div class="large">120</div>
-							<div class="text-muted">New Orders</div>
+							<div class="large">6</div>
+							<div class="text-muted">Barang baru</div>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 					<div class="panel panel-blue panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-comments color-orange"></em>
-							<div class="large">52</div>
-							<div class="text-muted">Comments</div>
+							<div class="large">2</div>
+							<div class="text-muted">Permintaan</div>
 						</div>
 					</div>
 				</div>
 				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 					<div class="panel panel-orange panel-widget border-right">
 						<div class="row no-padding"><em class="fa fa-xl fa-users color-teal"></em>
-							<div class="large">24</div>
+							<div class="large">2</div>
 							<div class="text-muted">New Users</div>
 						</div>
 					</div>
@@ -36,12 +36,18 @@
 				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 					<div class="panel panel-red panel-widget ">
 						<div class="row no-padding"><em class="fa fa-xl fa-search color-red"></em>
-							<div class="large">25.2k</div>
+							<div class="large">5</div>
 							<div class="text-muted">Page Views</div>
 						</div>
 					</div>
 				</div>
 			</div><!--/.row-->
+
+			<div class="panel panel-container">
+			<br>
+				<h3>Dedicated for Pertamina Lubricant</h3>
+				<img src="<?php echo base_url('assets/images/logoPL.png')?>" alt="Pertamina Lubricant">
+			</div>
 		</div>
 	</div>
 </div>
